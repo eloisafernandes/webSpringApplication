95ef333c7943111a8582868452f18c47a996723f
eloisafernandes <eloisafer16@gmail.com>
2024-03-12 13:03:59 +0000

    first application using Spring Boot

---
+++ src/test/java/br/edu/imd/helloword2/Helloword2ApplicationTests.java
@@ -0,0 +1,13 @@
+package br.edu.imd.helloword2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Helloword2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
